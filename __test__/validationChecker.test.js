eedb0efef0e67d73a6c4cf9d952bb81c8bc5d377
benoit motnes <benoit.montes@gmail.com>
2022-03-30 22:32:46 +0200

    jest installed and tests passed

--- __test__/validationChecker.test.js
+++ __test__/validationChecker.test.js
@@ -1,7 +1,24 @@
 import { checkUrl } from "../src/client/js/validationChecker";
 
 describe('Testing the "checkUrl" functionality', () => {
+    const url_empty = "";
+    const url_valid = "https://www.udacity.com/";
+    const url_invalid = "httpz:/ww.udacity";
+
+
     test('Testing the function exists', () => {
         expect(checkUrl).toBeDefined();
+    })
+
+    test('Testing returns true when provided a valid url', () => {
+        expect(checkUrl(url_valid)).toBeTruthy();
+    })
+
+    test('Testing returns false when provided a invalid url', () => {
+        expect(checkUrl(url_invalid)).toBeFalsy();
+    })
+
+    test('Testing returns false when provided a empty url', () => {
+        expect(checkUrl(url_empty)).toBeFalsy();
     })
 });
