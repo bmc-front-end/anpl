fe7492bea612bf14b0b5a0ef42261ccf06181039
benoit motnes <benoit.montes@gmail.com>
2022-03-31 18:01:03 +0200

    webpack config files updated maar bugggy

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+//const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+//const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -21,12 +22,18 @@ module.exports = {
         }
         ]
     },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+      //  filename: 'main.js',
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new CleanWebpackPlugin({
+/*         new CleanWebpackPlugin({
             // Simulate the removal of files
             dry: true,
             // Write Logs to Console
@@ -34,6 +41,7 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }), */
+      // new WorkboxPlugin.GenerateSW()
     ]
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 //const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
@@ -15,7 +18,8 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+              //  use: [ 'style-loader', 'css-loader', 'sass-loader' ]
         }
         ]
     },
@@ -25,11 +29,15 @@ module.exports = {
         libraryTarget: 'var',
         library: 'Client'
     },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
      //   new WorkboxPlugin.GenerateSW()
     ]
 }
